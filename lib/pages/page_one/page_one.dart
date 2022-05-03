c73ecb9606ad81387ab6c957d81aa651430fa56f
widjos <widjos@users.noreply.github.com>
2022-05-03 02:50:51 -0600

    new log in method

--- lib/pages/page_one/page_one.dart
+++ lib/pages/page_one/page_one.dart
@@ -2,8 +2,10 @@ import 'dart:io';
 
 import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:test/bloc/basic_bloc/basic_bloc.dart';
 import 'package:test/pages/page_two/page_two.dart';
@@ -13,9 +15,10 @@ import 'package:test/provider/sinister_provider.dart';
 import 'package:test/repository/sinister_repository.dart';
 import 'package:test/widgets/button_icon.dart';
 import 'package:test/widgets/gradient_back.dart';
-import 'package:test/widgets/text_input.dart';
+import 'package:test/widgets/text_input.dart' as myInput;
 import 'package:test/widgets/button_green.dart';
 import 'package:flutter_offline/flutter_offline.dart';
+import 'package:local_auth/local_auth.dart';
 
 // ignore: must_be_immutable
 class PageOne extends StatefulWidget {
@@ -30,8 +33,9 @@ class PageOne extends StatefulWidget {
 
 class _PageOneState extends State<PageOne> {
   final _textControllerUserName = TextEditingController();
-
   final _textControllerPassword = TextEditingController();
+  static final _auth = LocalAuthentication();
+  bool rememberMe = false;
 
   @override
   Widget build(BuildContext context) {
@@ -99,7 +103,7 @@ class _PageOneState extends State<PageOne> {
                               ),
                               Container(
                                 margin: const EdgeInsets.only(bottom: 20.0),
-                                child: TextInput(
+                                child: myInput.TextInput(
                                     hintText: 'Nombre de Usuario',
                                     inputType: null,
                                     controller: _textControllerUserName,
@@ -107,7 +111,7 @@ class _PageOneState extends State<PageOne> {
                               ),
                               Container(
                                 margin: const EdgeInsets.only(bottom: 20.0),
-                                child: TextInput(
+                                child: myInput.TextInput(
                                   hintText: 'Password',
                                   inputType: null,
                                   controller: _textControllerPassword,
@@ -115,53 +119,125 @@ class _PageOneState extends State<PageOne> {
                                   maxLines: 1,
                                 ),
                               ),
-                              widget.connected ? 
-                                Container(
-                                  width: 70,
-                                  child: ButtonGreen('Ingresar Online', () {
-                                    if (_textControllerUserName
-                                            .text.isNotEmpty &&
-                                        _textControllerPassword
-                                            .text.isNotEmpty) {
-                                      if (emailValid(
-                                          _textControllerUserName.text)) {
-                                        BlocProvider.of<BasicBloc>(context).add(
-                                            LoginSpring(
-                                                email: _textControllerUserName
-                                                    .text,
-                                                pass: _textControllerPassword
-                                                    .text));
-                                      } else {
-                                        BlocProvider.of<BasicBloc>(context)
-                                            .add(EmailInvalid());
-                                      }
-                                    }
-                                  }, 300.0, 50.0))
+                              Container(
+                                  margin: const EdgeInsets.only(bottom: 10.0),
+                                  child: Row(
+                                    children: [
+                                      Checkbox(
+                                        checkColor: Colors.white,
+                                        fillColor:
+                                            MaterialStateProperty.resolveWith(
+                                                getColor),
+                                        value: rememberMe,
+                                        onChanged: (bool? value) {
+                                          setState(() {
+                                            rememberMe = value!;
+                                          });
+                                        },
+                                      ),
+                                      const Text(
+                                        'Recordar mis credenciales',
+                                        style: TextStyle(
+                                            fontSize: 15.0,
+                                            fontFamily: "Lato",
+                                            color: Colors.blueGrey,
+                                            fontWeight: FontWeight.bold),
+                                      )
+                                    ],
+                                  )),
+                              widget.connected
+                                  ? Container(
+                                      width: 70,
+                                      child: ButtonGreen('Ingresar Online',
+                                          () async {
+                                        if (_textControllerUserName
+                                                .text.isNotEmpty &&
+                                            _textControllerPassword
+                                                .text.isNotEmpty) {
+                                          if (emailValid(
+                                              _textControllerUserName.text)) {
+                                            final prefs =
+                                                await SharedPreferences
+                                                    .getInstance();
+                                            /*  BlocProvider.of<BasicBloc>(context)
+                                                .add(LoginSpring(
+                                                    email:
+                                                        _textControllerUserName
+                                                            .text,
+                                                    pass:
+                                                        _textControllerPassword
+                                                            .text)); */
+                                            if (prefs.getString('email') !=
+                                                    null ) {
+                                                        _textControllerUserName.text = prefs.getString('email') ?? '';
+                                                                                                                _textControllerUserName.text = prefs.getString('email') ?? '';
+                                                           
+                                                      final isAutho = await authenticate();
+                                                      if (isAutho){
+                                                      _textControllerPassword.text = prefs.getString('pass') ?? ''; 
+                                                       BlocProvider.of<BasicBloc>(context)
+                                                .add(LoginSpring(
+                                                    email:
+                                                        _textControllerUserName
+                                                            .text,
+                                                    pass:
+                                                        _textControllerPassword
+                                                            .text));  
+                                                      }
+                                            } else {
+                                              if (rememberMe) {
+                                                prefs.setString(
+                                                    'email',
+                                                    _textControllerUserName
+                                                        .text);
+                                                prefs.setString(
+                                                    'pass',
+                                                    _textControllerPassword
+                                                        .text);
+                                                BlocProvider.of<BasicBloc>(context)
+                                                .add(LoginSpring(
+                                                    email:
+                                                        _textControllerUserName
+                                                            .text,
+                                                    pass:
+                                                        _textControllerPassword
+                                                            .text));         
+                                              }
+                                            }
+                                          } else {
+                                            BlocProvider.of<BasicBloc>(context)
+                                                .add(EmailInvalid());
+                                          }
+                                        }
+                                      }, 300.0, 50.0))
                                   : Container(
-                                  width: 70,
-                                  child: ButtonGreen('Ingresar offLine', () {
-                                    if (_textControllerUserName
-                                            .text.isNotEmpty &&
-                                        _textControllerPassword
-                                            .text.isNotEmpty) {
-                                      if (emailValid(
-                                          _textControllerUserName.text)) {
-                                        BlocProvider.of<BasicBloc>(context).add(
-                                            LoginDb(
-                                                email: _textControllerUserName
-                                                    .text,
-                                                pass: _textControllerPassword
-                                                    .text));
-                                      } else {
-                                        BlocProvider.of<BasicBloc>(context)
-                                            .add(EmailInvalid());
-                                      }
-                                    }
-                                  }, 300.0, 50.0)),
+                                      width: 70,
+                                      child:
+                                          ButtonGreen('Ingresar offLine', () {
+                                        if (_textControllerUserName
+                                                .text.isNotEmpty &&
+                                            _textControllerPassword
+                                                .text.isNotEmpty) {
+                                          if (emailValid(
+                                              _textControllerUserName.text)) {
+                                            BlocProvider.of<BasicBloc>(context)
+                                                .add(LoginDb(
+                                                    email:
+                                                        _textControllerUserName
+                                                            .text,
+                                                    pass:
+                                                        _textControllerPassword
+                                                            .text));
+                                          } else {
+                                            BlocProvider.of<BasicBloc>(context)
+                                                .add(EmailInvalid());
+                                          }
+                                        }
+                                      }, 300.0, 50.0)),
                             ],
                           )),
                       Container(
-                        alignment: Alignment.bottomCenter,
+                          alignment: Alignment.bottomCenter,
                           child: widget.connected
                               ? ButtonIcon(true, Icons.wifi, 20,
                                   Theme.of(context).primaryColor, () {})
@@ -183,26 +259,63 @@ class _PageOneState extends State<PageOne> {
         .hasMatch(email);
   }
 
-
   void checkDataConfiguration(bool connected) async {
-    if(connected){
-     // await  _deleteDb();
+    if (connected) {
+      // await  _deleteDb();
       await SinisterProvider.shared.initSinistroTable();
       await ClientProvider.shared.initClientDb();
       await InsuranceProvider.shared.initInsuranceDb();
-
-    }else{
-       print(await SinisterRepository.shared.showTable(table: 'siniestro'));
+    } else {
+      print(await SinisterRepository.shared.showTable(table: 'siniestro'));
     }
-
   }
 
-    Future<void> _deleteDb() async {
+  Future<void> _deleteDb() async {
     Directory directoryDb = await getApplicationDocumentsDirectory();
     String path = "${directoryDb.path}test_db";
     databaseFactory.deleteDatabase(path);
   }
 
+  Color getColor(Set<MaterialState> states) {
+    const Set<MaterialState> interactiveStates = <MaterialState>{
+      MaterialState.pressed,
+      MaterialState.hovered,
+      MaterialState.focused,
+    };
+    if (states.any(interactiveStates.contains)) {
+      return Colors.blue;
+    }
+    return Colors.red;
+  }
+
+  Future<bool> authenticate() async {
+    final isAvalible = await hasBiometrics();
+    if (!isAvalible) return false;
+    try {
+      return await _auth.authenticate(
+        localizedReason: 'Escaner de la huella dactilar',
+        options: const AuthenticationOptions(
+            useErrorDialogs: true, stickyAuth: true, biometricOnly: true),
+      );
+    } on PlatformException catch (e) {
+      print(e);
+      return false;
+    }
+  }
+
+  static Future<bool> hasBiometrics() async {
+    try {
+      return await _auth.canCheckBiometrics;
+    } on PlatformException catch (e) {
+      return false;
+    }
+  }
 }
 
-/** */
+
+/**                                        
+                                         
+                                         
+                                         
+
+*/
